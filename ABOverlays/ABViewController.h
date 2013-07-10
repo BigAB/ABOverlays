107fd361204d8249a1a8a2101bcf11a3cc5a4360
BigAB <abarrett@gasbuddy.com>
2013-07-10 11:04:16 -0600

    Initial Commit

---
+++ ABOverlays/ABViewController.h
@@ -0,0 +1,13 @@
+//
+//  ABViewController.h
+//  ABOverlays
+//
+//  Created by Adam Barrett on 2013-07-10.
+//  Copyright (c) 2013 Adam Barrett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABViewController : UIViewController
+
+@end
---
+++ ABOverlaysTests/ABOverlaysTests.h
@@ -0,0 +1,13 @@
+//
+//  ABOverlaysTests.h
+//  ABOverlaysTests
+//
+//  Created by Adam Barrett on 2013-07-10.
+//  Copyright (c) 2013 Adam Barrett. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ABOverlaysTests : SenTestCase
+
+@end
